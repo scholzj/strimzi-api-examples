86a6727997d0a7489ba52a7aeef1051546c283a4
Jakub Scholz <www@scholzj.com>
2022-07-23 21:48:36 +0200

    Update installation example
    Signed-off-by: Jakub Scholz <www@scholzj.com>

---
+++ src/main/java/cz/scholz/strimzi/api/examples/installation/Install.java
@@ -0,0 +1,110 @@
+package cz.scholz.strimzi.api.examples.installation;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.EnvVar;
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.NamespaceBuilder;
+import io.fabric8.kubernetes.api.model.ServiceAccount;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.rbac.ClusterRole;
+import io.fabric8.kubernetes.api.model.rbac.ClusterRoleBinding;
+import io.fabric8.kubernetes.api.model.rbac.ClusterRoleBindingBuilder;
+import io.fabric8.kubernetes.api.model.rbac.RoleBinding;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+public class Install {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Install.class);
+
+    private static final String INSTALLATION_YAML = "https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.30.0/strimzi-cluster-operator-0.30.0.yaml";
+    private static final String OPERATOR_NAMESPACE = "strimzi";
+    private static final boolean WATCH_ALL_NAMESPACES = true; // When set to true, the operator will watch all namespaces. When set to false, it will watch only the OPERATOR_NAMESPACE namespace
+
+    public static void main(String[] args) {
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+            LOGGER.info("Checking if namespace {} exists", OPERATOR_NAMESPACE);
+            if (client.namespaces().withName(OPERATOR_NAMESPACE).get() == null) {
+                LOGGER.info("Creating namespace {}", OPERATOR_NAMESPACE);
+                client.namespaces().create(new NamespaceBuilder().withNewMetadata().withName(OPERATOR_NAMESPACE).endMetadata().build());
+            }
+
+            LOGGER.info("Loading installation files from {}", INSTALLATION_YAML);
+            List<HasMetadata> resources = client.load(new BufferedInputStream(new URL(INSTALLATION_YAML).openStream())).get();
+
+            for (HasMetadata resource : resources) {
+                if (resource instanceof ServiceAccount) {
+                    LOGGER.info("Creating {} named {} in namespace {}", resource.getKind(), resource.getMetadata().getName(), OPERATOR_NAMESPACE);
+                    resource.getMetadata().setNamespace(OPERATOR_NAMESPACE);
+                    ServiceAccount sa = (ServiceAccount) resource;
+                    client.serviceAccounts().inNamespace(OPERATOR_NAMESPACE).createOrReplace(sa);
+                } else if (resource instanceof ClusterRole) {
+                    LOGGER.info("Creating {} named {}", resource.getKind(), resource.getMetadata().getName());
+                    ClusterRole cr = (ClusterRole) resource;
+                    client.rbac().clusterRoles().createOrReplace(cr);
+                } else if (resource instanceof ClusterRoleBinding) {
+                    LOGGER.info("Creating {} named {}", resource.getKind(), resource.getMetadata().getName());
+                    ClusterRoleBinding crb = (ClusterRoleBinding) resource;
+                    crb.getSubjects().forEach(sbj -> sbj.setNamespace(OPERATOR_NAMESPACE));
+                    client.rbac().clusterRoleBindings().createOrReplace(crb);
+                } else if (resource instanceof RoleBinding) {
+                    resource.getMetadata().setNamespace(OPERATOR_NAMESPACE);
+                    RoleBinding rb = (RoleBinding) resource;
+                    rb.getSubjects().forEach(sbj -> sbj.setNamespace(OPERATOR_NAMESPACE));
+
+                    if (WATCH_ALL_NAMESPACES) {
+                        ClusterRoleBinding crb = new ClusterRoleBindingBuilder()
+                                .withNewMetadata()
+                                .withName(rb.getMetadata().getName() + "-all-ns")
+                                .withAnnotations(rb.getMetadata().getAnnotations())
+                                .withLabels(rb.getMetadata().getLabels())
+                                .endMetadata()
+                                .withRoleRef(rb.getRoleRef())
+                                .withSubjects(rb.getSubjects())
+                                .build();
+
+                        LOGGER.info("Creating {} named {}", crb.getKind(), crb.getMetadata().getName());
+                        client.rbac().clusterRoleBindings().createOrReplace(crb);
+                    } else {
+                        LOGGER.info("Creating {} named {} in namespace {}", resource.getKind(), resource.getMetadata().getName(), OPERATOR_NAMESPACE);
+                        client.rbac().roleBindings().inNamespace(OPERATOR_NAMESPACE).createOrReplace(rb);
+                    }
+                } else if (resource instanceof CustomResourceDefinition) {
+                    LOGGER.info("Creating {} named {}", resource.getKind(), resource.getMetadata().getName());
+                    CustomResourceDefinition crd = (CustomResourceDefinition) resource;
+                    client.apiextensions().v1().customResourceDefinitions().createOrReplace(crd);
+                } else if (resource instanceof ConfigMap) {
+                    LOGGER.info("Creating {} named {} in namespace {}", resource.getKind(), resource.getMetadata().getName(), OPERATOR_NAMESPACE);
+                    resource.getMetadata().setNamespace(OPERATOR_NAMESPACE);
+                    ConfigMap cm = (ConfigMap) resource;
+                    client.configMaps().inNamespace(OPERATOR_NAMESPACE).createOrReplace(cm);
+                } else if (resource instanceof Deployment) {
+                    LOGGER.info("Creating {} named {} in namespace {}", resource.getKind(), resource.getMetadata().getName(), OPERATOR_NAMESPACE);
+                    resource.getMetadata().setNamespace(OPERATOR_NAMESPACE);
+                    Deployment dep = (Deployment) resource;
+
+                    if (WATCH_ALL_NAMESPACES) {
+                        Container container = dep.getSpec().getTemplate().getSpec().getContainers().stream().filter(cont -> "strimzi-cluster-operator".equals(cont.getName())).findFirst().orElseThrow();
+                        EnvVar envVar = container.getEnv().stream().filter(env -> "STRIMZI_NAMESPACE".equals(env.getName())).findFirst().orElseThrow();
+                        envVar.setValueFrom(null);
+                        envVar.setValue("*");
+                    }
+
+                    client.apps().deployments().inNamespace(OPERATOR_NAMESPACE).createOrReplace(dep);
+                } else {
+                    LOGGER.info("Unknown resource {} named {}", resource.getKind(), resource.getMetadata().getName());
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error("Something went wrong", e);
+        }
+    }
+}
